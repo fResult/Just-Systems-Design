81279224c96edad0cbff5b97aad90e4ba1f52164
fResult <styxmaz@gmail.com>
2024-06-29 21:58:46 +0700

    add hash-utils.ts

--- 11_hashing/hash-utils.ts
+++ 11_hashing/hash-utils.ts
@@ -1,1 +1,22 @@
-console.log("Hashing Example:")
+function hashString(str: string): number {
+  let hash = 0
+
+  if (str.length === 0) return hash
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i)
+    hash = ((hash << 5) - hash) + char
+    hash = hash & hash
+  }
+
+  return hash
+}
+
+function computeScore(username: string, server: string): number {
+  const usernameHash = hashString(username)
+  const serverHash = hashString(server)
+
+  return (usernameHash * 13 + serverHash * 11) % 67
+}
+
+export { hashString, computeScore }
